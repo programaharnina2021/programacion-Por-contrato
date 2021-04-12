0a8df9cff39cd3f777b60acd36f5572ae03d6812
programacion <>
2021-04-12 13:37:37 +0200

    telegrama

---
+++ 01 Teoria/src/telegrama03/Telegrama.java
@@ -0,0 +1,25 @@
+package telegrama03;
+
+public class Telegrama {
+
+
+	public double calcularTelegrama(char tipoTelegrama, int numPalabras) {
+		double costo;
+		// Si el tipo de telegrama es ordinario
+		if(tipoTelegrama == 'O' || tipoTelegrama == 'o')
+			// Si el número de palabras es menor o igual a 10
+			if(numPalabras <= 10) costo = 25.0f;
+		// Si el número de palabras es mayor a 10
+			else costo = 25.0f + 5.0f * (numPalabras - 10);
+		// Si el tipo de telegrama es urgente
+		else if(tipoTelegrama == 'U' || tipoTelegrama == 'u')
+			// Si el número de palabras es menor o igual a 10
+			if(numPalabras <= 10) costo = 40.0f;
+		// Si el número de palabras es mayor a 10
+			else costo = 40.0f + 7.5f * (numPalabras - 10);
+		else costo = 0;
+		// Escribe el costo del telegrama
+		return costo;
+	}
+	
+}
